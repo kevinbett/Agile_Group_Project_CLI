e9a771de812c04c86476dc1e0f63afe73c6536a8
sam <sam@localhost.localdomain>
2018-03-02 10:30:49 +0300

    Create user_signup class

---
+++ user_signup.py
@@ -0,0 +1,9 @@
+class Register:
+    def __init__(self):
+        self.users = {}
+    def add_user(self,name,password,type):
+        self.user[name] = password
+        self.user[name].append(password)
+        self.user[name].append(type)
+        return {"Message":"User created successfully"}
+

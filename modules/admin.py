0213fecbc309606179679fc6221fd9ec304618ad
Eva <evajohnson714@gmail.com>
2018-03-02 11:09:36 +0300

    [finishes #155648799]add admin page

---
+++ modules/admin.py
@@ -0,0 +1,37 @@
+class Admin:
+def __init__(self):
+self.admin = {}
+
+def add_comment(self,commet):
+self.admim[name] = commet
+return {"message": "Comment added sucesfully"}
+
+def delete_comment(self,name):
+del self.admin[name]
+return {"message": "comment deleted sucesfully"}
+
+
+def view_comment(self):
+return self.admin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+

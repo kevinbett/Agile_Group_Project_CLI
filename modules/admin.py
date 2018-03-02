b6f0c7c66b0095692671ea5fa5745fbb6febfd4a
kevinbett <bettkevin757@gmail.com>
2018-03-02 11:06:04 +0300

    Adds admin.py filie

---
+++ modules/admin.py
@@ -0,0 +1,27 @@
+class Users:
+    def __init__(self):
+        self.users = {}
+        self.password = password
+        self.userType = utype
+    def add_user(self,name,password,userType):
+        self.user[name] = password
+        self.user[name].append(password)
+        self.user[name].append(userType)
+        return {"Message":"User created successfully"}
+
+
+class Admin:
+	def __init__(self):
+		self.admin = {}
+
+	def add_comment(self,commet):
+		self.admim[name] = commet
+		return {"message": "Comment added sucesfully"}
+
+	def delete_comment(self,name):
+		del self.admin[name]
+		return {"message": "comment deleted sucesfully"}
+
+
+	def view_comment(self):
+		return self.admin

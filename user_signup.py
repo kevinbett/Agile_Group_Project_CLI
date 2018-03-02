26366ede5190da6b2cd86ca4b4d20c35e6472c8c
sam <sam@localhost.localdomain>
2018-03-02 10:42:57 +0300

    Initialise variables on user create

--- user_signup.py
+++ user_signup.py
@@ -1,9 +1,11 @@
 class Users:
     def __init__(self):
         self.users = {}
-    def add_user(self,name,password,type):
+        self.password = password
+        self.userType = utype
+    def add_user(self,name,password,userType):
         self.user[name] = password
         self.user[name].append(password)
-        self.user[name].append(type)
+        self.user[name].append(userType)
         return {"Message":"User created successfully"}
 

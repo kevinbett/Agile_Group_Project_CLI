aab2abd98d86eed5ad5b5ae1c93db587b5f7aea1
eyansky <ianeyansky@gmail.com>
2018-03-02 11:49:56 +0300

    login page

--- login.py
+++ login.py
@@ -1,15 +1,16 @@
 from user_signup import Users
 
-username = input("Enter your username: ")
-password = input("Enter your password: ")
+def login():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
 
-def login(username,password):
     for user in Users:
         if user == username:
             return "Welcome, you are logged in"
         else:
             return "wrong password"
 
+login()
 
                 
 

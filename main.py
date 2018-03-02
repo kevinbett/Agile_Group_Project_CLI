12f075ef9c05a6bba6f38efd7daae047a0722b46
kevinbett <bettkevin757@gmail.com>
2018-03-02 11:11:42 +0300

    Adds main.py file

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Usage:
+main.py>> add_commet ...
+main.py>> delete_comment 
+
+Options:
+    add_comment          : Adds a comment
+    del_comment         : Deletes comment
+
+    -i, --interactive  Interactive Mode
+    -h, --help  Show this screen and exit.
+"""

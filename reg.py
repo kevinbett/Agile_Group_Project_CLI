dcb440bc626234fe751a5fe464d6df6ab9073a98
eyansky <ianeyansky@gmail.com>
2018-03-02 12:46:50 +0300

    registration page

---
+++ reg.py
@@ -0,0 +1,10 @@
+from user_signup import Users
+
+def reg():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+
+    user.add['username']= ['password']
+    return "user details added"
+    
+reg()

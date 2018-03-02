7e8be6f10a9898bd2a48fc00f2f9e94437babd33
Samson Anami <samsonanami@gmail.com>
2018-03-02 13:09:05 +0300

    Update test add comment file

--- test_add_comment.py
+++ test_add_comment.py
@@ -4,5 +4,5 @@ from add_comment import AddComment
 class AddCommentTestCase(unittest.TestCase):
     def test_add_comment(self):
         add_comment_temp = AddComment()
-        response = AddComment.add_comment(self,1,"This is my comment")
+        response = add_comment_temp.add_comment(1,"This is my comment")
         self.assertEqual(response["Message"],"Comment added successfully")
